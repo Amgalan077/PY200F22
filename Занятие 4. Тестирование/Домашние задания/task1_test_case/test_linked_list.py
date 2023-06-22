662d29af6a197c43312b2415bb706648c15fc8c8
Amgalan077 <amgalan.07@bk.ru>
2023-06-22 16:49:53 +0300

    lesson4

--- Занятие 4. Тестирование/Домашние задания/task1_test_case/test_linked_list.py
+++ Занятие 4. Тестирование/Домашние задания/task1_test_case/test_linked_list.py
@@ -1,10 +1,48 @@
 import unittest
 from linked_list import LinkedList
 
-
+# def foo(func):
+#     def wrapper(*args,**kwargs):
+#         list_test = [1,2,3]
+#         ll = LinkedList(list_test)
+#         return func()
+#     return wrapper
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_append(self):
+        """Проверить, что элемент успешно добавляется в конец связного списка, и увеличивается длина списка"""
+        list_test = [1, 2, 3]
+        ll = LinkedList(list_test)
+        ll.append(4)
+        list_test.append(4)
+        self.assertEqual(list(ll), list_test)  # list(ll)
+
+    def test_getitem(self):
+        """Проверить, что возвращается правильное значение узла по указанному индексу."""
+        list_test = [1, 2, 3]
+        ll = LinkedList(list_test)
+        self.assertEqual(ll[1], list_test[1])
+
+    def test_setitem(self):
+        """Проверить, что узел по указанному индексу правильно изменяется на заданное значение"""
+        list_test = [1, 2, 3]
+        ll = LinkedList(list_test)
+        ll[1] = 100
+        list_test[1] = 100
+        self.assertEqual(list(ll), list_test)
+
+    def test_to_list(self):
+        """Проверить, что возвращается список со значениями всех узлов связного списка."""
+        list_test = [1, 2, 3]
+        ll = LinkedList(list_test)
+
+        self.assertEqual(ll.to_list(), list_test)
+
+    def test_add(self):
+        list_test = [1, 2, 3]
+        ll = LinkedList(list_test)
+        ll += [4,5,6]
+        list_test += [4,5,6]
+        self.assertEqual(ll.to_list(),list_test)
 
 
 if __name__ == '__main__':
